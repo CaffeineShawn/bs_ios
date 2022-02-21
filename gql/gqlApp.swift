298743717ff59a19b66a5080f79166b9b2a587fb
CaffeineShawn <52215406+CaffeineShawn@users.noreply.github.com>
2022-02-21 15:30:00 +0800

    Initial Commit

---
+++ gql/gqlApp.swift
@@ -0,0 +1,17 @@
+//
+//  gqlApp.swift
+//  gql
+//
+//  Created by Shaw Young on 2022/2/21.
+//
+
+import SwiftUI
+
+@main
+struct gqlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
